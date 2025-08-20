5dbb69f0e9b33d8231b1d34f44dbfd115c47e367
Dani-dzoe <danieldzotepe568@gmail.com>
2025-08-20 17:35:03 +0000

    Integrated cloudinary for file storage

---
+++ Backend/src/utils/cloudinary.ts
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { env } from '../config/env.js';
+
+cloudinary.config({
+  cloud_name: env.CLOUDINARY_CLOUD_NAME,
+  api_key: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET
+});
+
+export async function uploadImage(buffer: Buffer, folder = 'topman') {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream({ folder }, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    });
+    stream.end(buffer);
+  });
+}
