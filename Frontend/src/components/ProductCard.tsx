bf88a3d2c66e4257060594bcbfa7b57f36538871
Dani-dzoe <danieldzotepe568@gmail.com>
2025-11-05 07:03:58 +0000

    temp images added

--- Frontend/src/components/ProductCard.tsx
+++ Frontend/src/components/ProductCard.tsx
@@ -8,7 +8,7 @@ export default function ProductCard({ _id, name, price, image }: Product) {
         <div className="aspect-[4/3] w-full overflow-hidden rounded-xl bg-gray-100">
           {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
           {/* @ts-ignore demo placeholder */}
-          <img src={image || `https://picsum.photos/seed/${_id}/600/450`} alt={name} className="h-full w-full object-cover" />
+          <img src={`/assets/images/${product.category}.jpg` || `https://picsum.photos/seed/${_id}/600/450`} alt={name} className="h-full w-full object-cover" />
         </div>
         <div className="mt-3">
           <h3 className="line-clamp-1 font-semibold">{name}</h3>
